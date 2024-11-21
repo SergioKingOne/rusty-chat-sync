b46080d1d77db8e6f615b41f02317f2cd1fd90ee
Sergio <sergiorobayorr@gmail.com>
2024-11-20 21:35:48 -0500

    change schema and types

--- terraform/dynamodb/tables.tf
+++ terraform/dynamodb/tables.tf
@@ -1,28 +1,34 @@
-resource "aws_dynamodb_table" "messages" {
+resource "aws_dynamodb_table" "chat" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "messageId"
-  range_key    = "timestamp"
+  hash_key     = "PK"
+  range_key    = "SK"
 
   attribute {
-    name = "messageId"
+    name = "PK"
     type = "S"
   }
 
   attribute {
-    name = "timestamp"
-    type = "N"
-  }
-
-  attribute {
-    name = "username"
+    name = "SK"
     type = "S"
   }
 
+  attribute {
+    name = "GSI1PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI1SK"
+    type = "S"
+  }
+
+  # GSI for accessing messages by conversation
   global_secondary_index {
-    name            = "UserMessagesIndex"
-    hash_key        = "username"
-    range_key       = "timestamp"
+    name            = "GSI1"
+    hash_key        = "GSI1PK"
+    range_key       = "GSI1SK"
     projection_type = "ALL"
   }
 
@@ -32,49 +38,12 @@ resource "aws_dynamodb_table" "messages" {
   }
 }
 
-resource "aws_dynamodb_table" "users" {
-  name         = "${var.dynamodb_table_name}-users"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "username"
-
-  attribute {
-    name = "username"
-    type = "S"
-  }
-
-  attribute {
-    name = "email"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "EmailIndex"
-    hash_key        = "email"
-    projection_type = "ALL"
-  }
-
-  tags = {
-    Environment = "dev"
-    Name        = "${var.dynamodb_table_name}-users"
-  }
+output "chat_table_name" {
+  description = "Name of the DynamoDB chat table"
+  value       = aws_dynamodb_table.chat.name
 }
 
-output "messages_table_name" {
-  description = "Name of the DynamoDB messages table"
-  value       = aws_dynamodb_table.messages.name
-}
-
-output "messages_table_arn" {
-  description = "ARN of the DynamoDB messages table"
-  value       = aws_dynamodb_table.messages.arn
-}
-
-output "users_table_name" {
-  description = "Name of the DynamoDB users table"
-  value       = aws_dynamodb_table.users.name
-}
-
-output "users_table_arn" {
-  description = "ARN of the DynamoDB users table"
-  value       = aws_dynamodb_table.users.arn
+output "chat_table_arn" {
+  description = "ARN of the DynamoDB chat table"
+  value       = aws_dynamodb_table.chat.arn
 }
